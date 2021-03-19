12e405c3e7f47b1663664ee5f40ca024dfa7a455
Bruno DELATTRE <delattreb@gmail.com>
2021-03-19 18:54:47 +0100

    Feature ok

--- AlexaRubanLed.ino
+++ AlexaRubanLed.ino
@@ -7,18 +7,19 @@
 #include "config.h"
 
 WiFiClient wifiClient;
-Espalexa espalexa;
-GButton touch(PIN_BUTTON_1, LOW_PULL, NORM_OPEN);
+GButton touch(PIN_BUTTON_TOUCH, LOW_PULL, NORM_OPEN);
 WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-//callback function prototype
-void colorLightChanged(uint8_t brightness, uint32_t rgb);
+//callback functions
+void deltaChanged(EspalexaDevice *dev);
+Espalexa espalexa;
+EspalexaDevice *epsilon;
 
-bool bbright;
 static int effect = 2;
 static uint8_t bright = BRIGHTNESS; //0-255
 static int speed = SPEED_EFFECT;
-uint32_t red, green, blue;
+uint8_t red = RED, green = GREEN, blue = BLUE;
+
 // setup
 void setup()
 {
@@ -59,30 +60,38 @@ void setup()
 	EEPROM.begin(EEPROM_SIZE);
 
 	//Input configuration
-	pinMode(PIN_BUTTON_1, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(PIN_BUTTON_1), pinDidChange, CHANGE);
+	pinMode(PIN_BUTTON_TOUCH, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(PIN_BUTTON_TOUCH), pinDidChange, CHANGE);
 	touch.setDebounce(80);
 	touch.setTimeout(200);
-	bbright = false;
 
 	if (EEPROM.read(EEPROM_PLACE_BRIGHT) >= 1)
 	{
 		bright = EEPROM.read(EEPROM_PLACE_BRIGHT);
+		red = EEPROM.read(EEPROM_PLACE_RED);
+		green = EEPROM.read(EEPROM_PLACE_GREEN);
+		blue = EEPROM.read(EEPROM_PLACE_BLUE);
+
 #ifdef DEBUG
 		Serial.print("Load EEPROM: ");
-		Serial.println(bright);
+		Serial.print("Bright");
+		Serial.print(bright);
+		Serial.print("R");
+		Serial.print(red);
+		Serial.print("G");
+		Serial.print(green);
+		Serial.print("B");
+		Serial.println(blue);
 #endif
 	}
-	else
-		bright = BRIGHTNESS;
 
-	//Define Alexa device name
-	espalexa.addDevice(DEVICE_NAME, colorLightChanged);
+	// Define your devices here.
+	espalexa.addDevice(DEVICE_NAME, deltaChanged, EspalexaDeviceType::color);
 	espalexa.begin();
 
 	//WS2812
 	ws2812fx.init();
-	ws2812fx.setColor(PURPLE);
+	ws2812fx.setColor(red, green, blue);
 	ws2812fx.setBrightness(bright);
 	ws2812fx.setMode(effect);
 	ws2812fx.setSpeed(speed);
@@ -92,6 +101,9 @@ void setup()
 // loop
 void loop()
 {
+	//Button gesture
+	touch.tick();
+
 	//Alexa
 	espalexa.loop();
 	delay(1);
@@ -99,43 +111,25 @@ void loop()
 	//WS2812
 	ws2812fx.service();
 
-	//Button gesture
-	touch.tick();
-
-	if (touch.isTriple())
+	if (touch.isDouble())
 	{
 #ifdef DEBUG
 		Serial.print("Save EEPROM: ");
-		Serial.println(bright);
+		Serial.print("Bright");
+		Serial.print(bright);
+		Serial.print("R");
+		Serial.print(red);
+		Serial.print("G");
+		Serial.print(green);
+		Serial.print("B");
+		Serial.println(blue);
 #endif
 		EEPROM.write(EEPROM_PLACE_BRIGHT, bright);
+		EEPROM.write(EEPROM_PLACE_RED, red);
+		EEPROM.write(EEPROM_PLACE_GREEN, green);
+		EEPROM.write(EEPROM_PLACE_BLUE, blue);
 		EEPROM.commit();
 	}
-	/*
-	if (touch.isStep())
-	{
-		if (bbright)
-		{
-			if (bright + INCREMENT < 255)
-				bright = bright + INCREMENT;
-		}
-		else
-		{
-			if (bright - INCREMENT > 5)
-				bright = bright - INCREMENT;
-		}
-#ifdef DEBUG
-		Serial.print("bright: ");
-		Serial.println(bright);
-#endif
-	}
-	if (touch.isRelease())
-	{
-		bbright = !bbright;
-#ifdef DEBUG
-		Serial.println("Change");
-#endif
-	}*/
 }
 
 //pinDidChange
@@ -144,27 +138,33 @@ ICACHE_RAM_ATTR void pinDidChange()
 	touch.tick();
 }
 
-//the color device callback function has two parameters
-void colorLightChanged(uint8_t brightness, uint32_t rgb)
+//Callback
+void deltaChanged(EspalexaDevice *d)
 {
-	red = (rgb >> 16) & 0xFF;
-	green = (rgb >> 8) & 0xFF;
-	blue = rgb & 0xFF;
+	if (d == nullptr)
+		return;
+	if (d->getName() == DEVICE_NAME)
+	{
+		//Get color
+		bright = d->getValue();
+		red = d->getR();
+		green = d->getG();
+		blue = d->getB();
 
-	ws2812fx.stop();
-	ws2812fx.setBrightness(uint8_t(brightness));
-	ws2812fx.start();
-
+		//Set new color
+		ws2812fx.stop();
+		ws2812fx.setColor(red, green, blue);
+		ws2812fx.setBrightness(bright);
+		ws2812fx.start();
 #ifdef DEBUG
-	Serial.print("Brightness: ");
-	Serial.print(brightness);
-	Serial.print(", rgb: ");
-	Serial.print(rgb);
-	Serial.print(", Red: ");
-	Serial.print((rgb >> 16) & 0xFF); //get red component
-	Serial.print(", Green: ");
-	Serial.print((rgb >> 8) & 0xFF); //get green
-	Serial.print(", Blue: ");
-	Serial.println(rgb & 0xFF); //get blue#endif
+		Serial.print("Value: ");
+		Serial.print(bright);
+		Serial.print(", color R");
+		Serial.print(red);
+		Serial.print(", G");
+		Serial.print(green);
+		Serial.print(", B");
+		Serial.println(blue);
 #endif
+	}
 }
